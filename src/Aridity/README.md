acf008204411ce94f2c41b59ccf153495ff648c4
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2024-03-16 20:02:23 +0800

    Visual Studio decided to fuck me in the ass
    Visual Studio decided to fuck me in the ass by breaking the main Aridity application project. Nothing new but some changes

--- src/Aridity/README.md
+++
@@ -1,2 +0,0 @@
-# Aridity
- Steam but for sourcemods. (If you have a better shit please let me know and ill delete this project. Excluding Adastral. Not a competitor to Adastral)
--- src/Aridity/Startup/App.xaml.cs
+++ src/Aridity/Startup/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,11 +14,17 @@ namespace Aridity
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-
+            if(e.Args.Contains("--console"))
+            {
+                AllocConsole();
+            }
         }
     }
 }
